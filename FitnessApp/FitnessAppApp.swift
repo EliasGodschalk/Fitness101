67056ec7c943427b6706d4bcf32f6c04a2f796cc
Elias Godschalk <elias.godschalk203@gmail.com>
2025-01-24 12:01:19 +0100

    Initial Commit

---
+++ FitnessApp/FitnessAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessAppApp.swift
+//  FitnessApp
+//
+//  Created by Elias Godschalk on 24.01.25.
+//
+
+import SwiftUI
+
+@main
+struct FitnessAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
