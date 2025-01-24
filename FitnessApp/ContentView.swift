ee14fcd6f3c922eeaf7be041a22f606ebf69ab52
Elias Godschalk <elias.godschalk203@gmail.com>
2025-01-24 12:29:23 +0100

    added Test Text

--- FitnessApp/ContentView.swift
+++ FitnessApp/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("SwiftUI course")
+            Text("Test text")
         }
         .padding()
     }
